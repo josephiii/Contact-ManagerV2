1eba0a62a301d504924845398ece300e6c0eface
Joseph Ales <162182688+josephiii@users.noreply.github.com>
2025-03-08 21:58:10 -0500

    foundation building 1

---
+++ sqlSetup.sql
@@ -0,0 +1,23 @@
+CREATE TABLE 'Reach' . 'Users' ( -- if no database exists, create one called Reach
+    id INT NOT NULL AUTO_INCREMENT, -- user ID that increments 
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL, -- hashed
+
+    primary key (id) -- no two users can have the same ID
+) Engine = InnoDB; 
+
+CREATE TABLE 'Reach' . 'Contacts' (
+
+    id INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(50) NOT NULL,
+    location VARCHAR(50), -- optional contact location field
+
+    userId INT NOT NULL, -- matches users to their contacts
+    primary key (id) -- no two contacts can have the same ID
+    foreign key (userId) references Users(id) on delete cascade -- if user is del, all contacts are del too
+) Engine = InnoDB;
+
